5ed338676191107259753213110963d8b4a70b93
merte_000 <merte_000@Baikonur.Elisa>
2014-01-02 20:53:51 +0200

    Vaihdettu kekototeutus toimimaan Noodeilla.

--- test/fi/tiralabra/astar/KekoTest.java
+++ test/fi/tiralabra/astar/KekoTest.java
@@ -44,45 +44,77 @@ public class KekoTest {
     @Test
     public void testLisaa() {
         System.out.println("lisaa");
-        Keko instance = new Keko(5);
-        instance.lisaa(6);
-        instance.lisaa(2);
-        instance.lisaa(8);
-        instance.lisaa(4);
-        instance.lisaa(12);
-        instance.lisaa(3);
-        instance.lisaa(7);
-        int paluu = instance.poistaPienin();
-        assertEquals(2, paluu);        
-        instance.lisaa(11);
-        instance.lisaa(9);
-        paluu = instance.poistaPienin();
-        assertEquals(3, paluu);
-        instance.lisaa(2);
-        paluu = instance.poistaPienin();
-        assertEquals(2, paluu);        
-        
+        Keko instance = new Keko(2);
+        Noodi noodi = new Noodi(0,0);
+        noodi.setMatkaJaljella(4);
+        Noodi noodi2 = new Noodi(0,1);
+        noodi2.setMatkaJaljella(6);
+        Noodi noodi3 = new Noodi(0,2);
+        noodi3.setMatkaJaljella(2);
+        instance.lisaa(noodi);
+        instance.lisaa(noodi2);
+        instance.lisaa(noodi3);
+        //Lisääminen ei kaatanut ohjelmaa vaikka maksimikoko ylitettiin
+        assertEquals(true, true);
         
     }
     
     /**
-     * Test of lisaa method, of class Keko.
+     * Test of poistaPienin method, of class Keko.
      */
     @Test
-    public void testLisaa2() {        
-        System.out.println("lisaa2");
+    public void testPoistaPienin() {
+        System.out.println("poistaPienin");
+        Keko instance = new Keko(2);
+        Noodi noodi = new Noodi(0,0);
+        noodi.setMatkaJaljella(4);
+        Noodi noodi2 = new Noodi(0,1);
+        noodi2.setMatkaJaljella(6);
+        Noodi noodi3 = new Noodi(0,2);
+        noodi3.setMatkaJaljella(2);
+        instance.lisaa(noodi);
+        instance.lisaa(noodi2);
+        instance.lisaa(noodi3);
+        Noodi paluu = instance.poistaPienin();
+        assertEquals(paluu.getMatkaaJaljella(), 2);
+        Noodi noodi4 = new Noodi(0,3);
+        noodi4.setMatkaJaljella(7);
+        Noodi noodi5 = new Noodi(0,4);
+        noodi5.setMatkaJaljella(5);   
+        instance.lisaa(noodi4);
+        instance.lisaa(noodi5);
+        paluu = instance.poistaPienin();
+        assertEquals(paluu.getMatkaaJaljella(), 4);
+        instance.lisaa(noodi3);
+        paluu = instance.poistaPienin();
+        assertEquals(paluu.getMatkaaJaljella(), 2);                        
+    }    
+    
+    /**
+     * Test of poistaPienin method, of class Keko.
+     */
+    @Test
+    public void testPoistaPienin2() {        
+        System.out.println("poistaPienin2");
         Keko instance = new Keko(100);
-        instance.lisaa(6);
-        instance.lisaa(8);
-        instance.lisaa(4);
-        instance.lisaa(2);
-        instance.lisaa(3);
-        instance.lisaa(5);
-        instance.lisaa(10);
-        instance.lisaa(7);
-        int temp = instance.poistaPienin();
-        int paluu = instance.poistaPienin();
-        assertEquals(3, paluu);        
+        Noodi noodi = new Noodi(0,0);
+        noodi.setMatkaJaljella(4);
+        Noodi noodi2 = new Noodi(0,1);
+        noodi2.setMatkaJaljella(6);
+        Noodi noodi3 = new Noodi(0,2);
+        noodi3.setMatkaJaljella(2);
+        Noodi noodi4 = new Noodi(0,3);
+        noodi4.setMatkaJaljella(7);
+        Noodi noodi5 = new Noodi(0,4);
+        noodi5.setMatkaJaljella(5);   
+        instance.lisaa(noodi4);
+        instance.lisaa(noodi5);        
+        instance.lisaa(noodi);
+        instance.lisaa(noodi2);
+        instance.lisaa(noodi3);        
+        Noodi temp = instance.poistaPienin();
+        Noodi paluu = instance.poistaPienin();
+        assertEquals(paluu.getMatkaaJaljella(), 4);     
     }   
     
     /**
@@ -92,44 +124,63 @@ public class KekoTest {
     public void testOnkoTyhja2() {
         System.out.println("onkoTyhja");
         Keko instance = new Keko(100);
-        instance.lisaa(6);
-        int temp = instance.poistaPienin();
-        assertEquals(true, instance.onkoTyhja());        
+        Noodi noodi = new Noodi(0,0);
+        noodi.setMatkaJaljella(4);
+        instance.lisaa(noodi);
+        Noodi temp = instance.poistaPienin();
+        assertEquals(true, instance.onkoTyhja());       
     }     
     
     @Test 
     public void testToString(){
-        System.out.println("toString");
+      System.out.println("toString");
         Keko instance = new Keko(100);
-        instance.lisaa(6);
-        instance.lisaa(8);
-        instance.lisaa(4);
-        instance.lisaa(2);
-        instance.lisaa(3);
-        instance.lisaa(5);
-        instance.lisaa(10);
-        instance.lisaa(7);
+        Noodi noodi = new Noodi(0,0);
+        noodi.setMatkaJaljella(4);
+        Noodi noodi2 = new Noodi(0,1);
+        noodi2.setMatkaJaljella(6);
+        Noodi noodi3 = new Noodi(0,2);
+        noodi3.setMatkaJaljella(2);
+        Noodi noodi4 = new Noodi(0,3);
+        noodi4.setMatkaJaljella(7);
+        Noodi noodi5 = new Noodi(0,4);
+        noodi5.setMatkaJaljella(5);   
+        instance.lisaa(noodi4);
+        instance.lisaa(noodi5);        
+        instance.lisaa(noodi);
+        instance.lisaa(noodi2);
+        instance.lisaa(noodi3);      
         
         System.out.println(instance.toString());
         
-        assertEquals(true, true);               
+        assertEquals(true, true);   
     }
     
     @Test
     public void testNaytaPienin(){
         System.out.println("naytaPienin");
         Keko instance = new Keko(100);
-        instance.lisaa(6);
-        instance.lisaa(8);
-        instance.lisaa(4);
-        instance.lisaa(2);
-        instance.lisaa(3);
-        instance.lisaa(5);
-        instance.lisaa(10);
-        instance.lisaa(7);
-        int temp = instance.poistaPienin();
-        int paluu = instance.naytaPienin();
-        assertEquals(3, paluu);        
-
+        Noodi noodi = new Noodi(0,0);
+        noodi.setMatkaJaljella(4);
+        Noodi noodi2 = new Noodi(0,1);
+        noodi2.setMatkaJaljella(6);
+        Noodi noodi3 = new Noodi(0,2);
+        noodi3.setMatkaJaljella(2);
+        Noodi noodi4 = new Noodi(0,3);
+        noodi4.setMatkaJaljella(7);
+        Noodi noodi5 = new Noodi(0,4);
+        noodi5.setMatkaJaljella(5);   
+        instance.lisaa(noodi4);
+        instance.lisaa(noodi5);        
+        instance.lisaa(noodi);
+        instance.lisaa(noodi2);
+        instance.lisaa(noodi3);              
+        Noodi temp = instance.poistaPienin();
+        Noodi paluu = instance.naytaPienin();
+        assertEquals(paluu.getMatkaaJaljella(), 4);
+        temp = instance.poistaPienin();
+        assertEquals(temp.getMatkaaJaljella(), 4);  
+        paluu = instance.naytaPienin();
+        assertEquals(paluu.getMatkaaJaljella(), 5);
     }    
 }
